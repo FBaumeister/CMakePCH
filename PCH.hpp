a56b26b7e0e3fb5a64bb7178dea6773aa75384cd
FBaumeister <fabian_baumeister@gmx.de>
2024-05-20 18:25:46 +0200

    Initial commit on PCH usage.

---
+++ PCH.hpp
@@ -0,0 +1,9 @@
+#ifndef PCH_HPP
+#define PCH_HPP
+
+#include <iostream>
+#include <memory>
+#include <vector>
+// ...
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,11 @@
+
+// Note: No include of PCH necessary here!
+
+int main()
+{
+    auto a = std::make_shared<int>();
+    std::vector<int> someVector;
+    // ...
+
+    return 0;
+}
